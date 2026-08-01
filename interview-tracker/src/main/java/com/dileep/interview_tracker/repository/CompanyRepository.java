1fa252e4a3fe97fc1306f39805cdbcb9f77d84c6
dileepdillu2006 <dileepsunny1234509876@gmail.com>
2026-08-01 09:37:23 +0530

    Delete interview-tracker/src directory

--- interview-tracker/src/main/java/com/dileep/interview_tracker/repository/CompanyRepository.java
+++
@@ -1,9 +0,0 @@
-package com.dileep.interview_tracker.repository;
-
-import com.dileep.interview_tracker.entity.Company;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-
-public interface CompanyRepository extends JpaRepository<Company, Long> {
-    List<Company> findByUserId(Long userId);
-}
